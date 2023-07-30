a6bbc30ee70d09eef50d2f2f7d951a78f093df04
NicoleHitter <nicole.hitter92@gmail.com>
2023-07-30 17:36:06 +0000

    Disable collestatic

--- sweet_home_cakes/settings.py
+++ sweet_home_cakes/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-=^jwj4&xpwf48mdk=#o!%lzsf=m785wk&$d47en$b_v==a5u=6
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
-DISABLE_COLLESTATIC= 'DISABLE_COLLESTATIC' in os.environ
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['NicoleHitter/sweet-home-cakes.herokuapp.com', 'localhost']
 
