78f4a44a47ab8d898b27f0e577809d3dba4f511e
NicoleHitter <nicole.hitter92@gmail.com>
2023-07-31 20:36:19 +0000

    Add heroku host

--- sweet_home_cakes/settings.py
+++ sweet_home_cakes/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-=^jwj4&xpwf48mdk=#o!%lzsf=m785wk&$d47en$b_v==a5u=6
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['NicoleHitter/sweet-home-cakes.herokuapp.com']
+ALLOWED_HOSTS = ['sweet-home-cakes-5f6937a7c043.herokuapp.com']
 
 
 # Application definition
